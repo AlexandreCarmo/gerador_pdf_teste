50a5f311aa8cce496d8b97c1b97424daf541edd4
Casagrande <alexandre.carmo@sou.ufmt.br>
2023-07-05 12:11:04 -0400

    CSS modifies

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello world!");
+const btnGenerate = document.querySelector("#generate-pdf-btn");
+
+
+// Events
+
+btnGenerate.addEventListener("click", () => {
+
+    //conteudo do pdf
+    const content = document.querySelector("#pdf-content");
+
+    //config do arquivo final
+    const options = {
+        margin: [10,10,10,10],
+        filename: "arquivo.pdf",
+        html2canvas: {scale: 2},
+        jsPDF: {unit: "mm", format: "a4", orientation: "portrait"}
+    }
+
+    //Gerar e baixar PDF
+    html2pdf().set(options).from(content).save();
+
+})
